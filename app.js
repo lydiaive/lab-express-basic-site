e204bb31d2f8545eff9ccb1cd7ef85c27cbc8db3
Ivana-Lida Heintze <lydiaive@gmail.com>
2022-10-25 16:57:50 +0200

    lab done without final touches

--- app.js
+++ app.js
@@ -11,13 +11,23 @@ app.get("/about", (request, response, next) => {
     response.sendFile( __dirname + "/views/About.html")
 }) 
 
-app.get("/works", (request, response, next) => {
+app.get("/work", (request, response, next) => {
     response.sendFile( __dirname + "/views/Works.html")
 }) 
+
 app.get("/photo-gallery", (request, response, next) => {
     response.sendFile( __dirname + "/views/Photo-Gallery.html")
+}) 
+
+app.get("/books", (request, response, next) => {
+    response.sendFile( __dirname + "/views/Books.html")
+}) 
+
+app.get("/surfing", (request, response, next) => {
+    response.sendFile( __dirname + "/views/Surfing-Career.html")
 }) 
 
 app.listen(3000, () => {
     console.log("server running")
 })
+
