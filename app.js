de6b1ebc907abbf2d98f45db72ffd9814c4e142b
Ivana-Lida Heintze <lydiaive@gmail.com>
2022-10-26 11:34:34 +0200

    adding navbar as a partial to books

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ app.set("view engine", "hbs")
 
 app.use(express.static("public"))
 
+hbs.registerPartials(__dirname + "/views/partials")
+
 app.get("/", (request, response) => {
     const person = {
         namePerson : "bethany.hamilton"
